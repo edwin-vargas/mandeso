ea34f385593dcd93cf36624e70c9ccea2d824940
edwin-vargas <edwin548vargas@gmail.com>
2025-05-18 16:27:06 -0600

    mongoose integration for mongoDB

--- server/app.js
+++ server/app.js
@@ -1,6 +1,19 @@
-import express from 'express'; // Changed from require('express')
+import express from 'express';
 const app = express();
 const port = 3000;
+
+const uri = "mongodb://mongoatia:apasswordGIv3n@localhost:27017/pinterest?authSource=admin";
+import mongoose from "mongoose";
+import User from "./model/users.js";
+mongoose.connect(uri);
+console.log("connected");
+
+const user = await User.create({
+  name: 'matacho',
+  email: 'email'
+});
+
+console.log(user)
 
 app.get('/helloworld', (req, res) => {
   res.send('Hello World!');
